a108c58c0c27e89b6374c96a97f9659134f66564
Erik van Velzen <erik@evanv.nl>
2023-07-10 12:55:20 +0200

    fix marker png and bounding box order

--- frontend/src/MainMap.tsx
+++ frontend/src/MainMap.tsx
@@ -1,14 +1,42 @@
 import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
-import {geoJson, LatLng, LatLngBounds, LatLngTuple} from "leaflet";
+import {geoJson, LatLng, LatLngBounds, Icon} from "leaflet";
 import "leaflet/dist/leaflet.css";
 import {useEffect, useLayoutEffect, useRef} from "react";
 import proj4 from "proj4";
+
+import markerIconPng from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2xPng from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadowPng from 'leaflet/dist/images/marker-shadow.png';
 
 const disruptorBuildingLocation = new LatLng(51.44971831403754, 5.4947035381928035)
 
 // definitie van de Nederlandse coördinatenstelsel
 // bron: iemand op het internet
 const RD = "+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs";
+
+// fix marker image (not sure we're ever gonna use this)
+// alternatively assign to Marker.prototype.options.icon.options
+Icon.Default.mergeOptions({
+    iconUrl: markerIconPng,
+    iconRetinaUrl: markerIcon2xPng,
+    shadowUrl: markerShadowPng,
+})
+
+function getBag3dData() {
+    const boundingBox = disruptorBuildingLocation.toBounds(300)
+
+    const params = new URLSearchParams({
+        request: "GetFeature",
+        typeName: "BAG3D:lod12",
+        count: "100",
+        crs: "EPSG:3857", // web mercator projection
+        outputFormat: "json",
+        srsName: "EPSG:4326", // GPS
+        bbox: boundingBoxToBag(boundingBox),
+    });
+
+    const url = 'https://data.3dbag.nl/api/BAG3D/wfs?' + params.toString();
+}
 
 async function addJsonToMap(mapRef: any) {
     const boundingBox = disruptorBuildingLocation.toBounds(300)
@@ -43,12 +71,13 @@ async function addJsonToMap(mapRef: any) {
 function boundingBoxToBag(boundingBox: LatLngBounds): string {
     const {x: x1, y: y1} = proj4("EPSG:4326", RD, {
         x: boundingBox.getWest(),
-        y: boundingBox.getNorth(),
+        y: boundingBox.getSouth(),
     })
     const {x: x2, y: y2} = proj4("EPSG:4326", RD, {
         x: boundingBox.getEast(),
-        y: boundingBox.getSouth(),
+        y: boundingBox.getNorth(),
     })
+
     return [x1, y1, x2, y2].join(",")
 }
 
