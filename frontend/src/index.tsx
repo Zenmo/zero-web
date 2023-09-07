46f7fd2da39ac9a2c4aa2947a38bd7102a428de3
Erik van Velzen <erik@evanv.nl>
2023-09-07 16:15:52 +0200

    Add react-router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+    },
+    {
+        path: "/bedrijven",
+        element: <div>Bedrijven</div>,
+    }
+]);
+
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement,
+    document.getElementsByTagName('body')[0],
 )
 root.render(
     <React.StrictMode>
-        <App/>
+        <RouterProvider router={router} />
     </React.StrictMode>,
 )
 
