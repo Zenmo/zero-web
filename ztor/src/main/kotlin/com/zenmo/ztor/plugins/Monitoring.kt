97b621283f3e38421367ff03397cafdffeacd5f2
Erik van Velzen <erik@evanv.nl>
2024-04-18 12:23:42 +0200

    Set CORS pattern for test env

--- ztor/src/main/kotlin/com/zenmo/ztor/plugins/Monitoring.kt
+++ ztor/src/main/kotlin/com/zenmo/ztor/plugins/Monitoring.kt
@@ -1,12 +1,41 @@
 package com.zenmo.ztor.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.callloging.*
 import io.ktor.server.request.*
 import org.slf4j.event.*
 
 fun Application.configureMonitoring() {
-    install(CallLogging) {
-        filter { call -> call.request.path().startsWith("/") }
+    install(CallLogging)
+//    {
+// Uncomment to log more
+//        format {
+//            extendedFormat(it)
+//        }
+//    }
+}
+
+fun extendedFormat(it: ApplicationCall): String = buildString {
+    append(it.request.httpMethod.value)
+    append(" ")
+    append(it.request.path())
+    appendLine()
+    addHeaders(it.request.headers)
+    appendLine()
+
+    append(it.response.status())
+    addHeaders(it.response.headers.allValues())
+}
+
+fun StringBuilder.addHeaders(headers: Headers) {
+    headers.forEach { key, values ->
+        values.forEach {
+            append("  ")
+            append(key)
+            append(": ")
+            append(it)
+            appendLine()
+        }
     }
 }
