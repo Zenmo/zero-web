993b9e26a319d9a80c3e33bc0fb381416c823c62
GillisHommen <98752265+GillisHommen@users.noreply.github.com>
2024-07-17 13:48:20 +0200

    Added zummon publish code

--- zummon/build.gradle.kts
+++ zummon/build.gradle.kts
@@ -1,10 +1,13 @@
+import java.net.URI
+
 plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization")
+    `maven-publish`
 }
 
 group = "com.zenmo"
-version = "0.0.1"
+version = System.getenv("VERSION_TAG") ?: "dev"
 
 repositories {
     mavenCentral()
@@ -28,6 +31,26 @@ kotlin {
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0-RC.2")
                 implementation("com.benasher44:uuid:0.8.4")
+            }
+        }
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.zenmo"
+            artifactId = "libzummon"
+            version = System.getenv("VERSION_TAG") ?: "dev"
+        }
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI("https://maven.pkg.github.com/zenmo/zero")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
             }
         }
     }
