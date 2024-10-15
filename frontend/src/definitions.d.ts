3fca240c46b04d3710d1943f2bcde93d94cf4ed6
Maria Cano <macano33@gmail.com>
2024-10-15 12:31:49 +0200

    Cleaning code

--- frontend/src/definitions.d.ts
+++ frontend/src/definitions.d.ts
@@ -1,12 +1,6 @@
 // silences typescript complaining about importing images
 declare module '*.png'
 
-declare var process: {
-    env: {
-        [key: string]: string | undefined
-    }
-}
-
 declare module 'memoize-immutable' {
     export default function memoize<Fn extends Function>(f: Fn): Fn
 }
