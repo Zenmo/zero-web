8e8b5edc100dd4b07b750a18498cef2c2d3f256f
kofi-k <kofivincent026@gmail.com>
2025-03-25 19:52:48 +0000

    frontend -> survey  datatable for index surveys  - minor ui things

--- frontend/src/components/zero-layout.tsx
+++ frontend/src/components/zero-layout.tsx
@@ -1,28 +1,36 @@
-import React, {FunctionComponent, PropsWithChildren} from "react"
+import React, {FunctionComponent, PropsWithChildren, ReactNode} from "react"
 
 export const ZeroLayout: FunctionComponent<PropsWithChildren & {
     subtitle?: string,
+    trailingContent?: ReactNode,
 }> = ({
     children,
     subtitle,
+    trailingContent,
 }) => (
     <div style={{
         minHeight: "100vh",
         display: "flex",
         flexDirection: "column",
     }}>
-        <h1 style={{
+        <div style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
             paddingLeft: "1em",
         }}>
-            <img
-                alt="Zenmo logo"
-                src="https://zenmo.com/wp-content/uploads/elementor/thumbs/zenmo-logo-website-light-grey-square-o1piz2j6llwl7n0xd84ywkivuyf22xei68ewzwrvmc.png"
-                style={{height: "1em", verticalAlign: "sub"}}/>
-            &nbsp;
-            Zenmo Zero
-        </h1>
+            <h1>
+                <img
+                    alt="Zenmo logo"
+                    src="https://zenmo.com/wp-content/uploads/elementor/thumbs/zenmo-logo-website-light-grey-square-o1piz2j6llwl7n0xd84ywkivuyf22xei68ewzwrvmc.png"
+                    style={{height: "1em", verticalAlign: "sub"}}/>
+                &nbsp;
+                Zenmo Zero
+            </h1>
+        {trailingContent && <div style={{ paddingRight: "1em" }}>{trailingContent}</div>}
+        </div>
         {subtitle && <h3 style={{
-            padding: "0 1em 1em 5rem",
+            padding: "0 1em 1em 1rem",
             margin: 0,
         }}>
             {subtitle}
@@ -30,4 +38,3 @@ export const ZeroLayout: FunctionComponent<PropsWithChildren & {
         {children}
     </div>
 )
-
