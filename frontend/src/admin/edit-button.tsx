1b05038bf77b74495549f0c489850cb5b11b848a
kofi-k <kofivincent026@gmail.com>
2025-04-11 14:55:30 +0000

    frontend: removed unused icons and icon component

--- frontend/src/admin/edit-button.tsx
+++ frontend/src/admin/edit-button.tsx
@@ -1,11 +1,10 @@
-import {Link} from "react-router-dom";
-import {ZeroIcon} from "../components/ZeroIcon";
+import {Link} from "react-router-dom"
 
-export const EditButton = ({type, id}: {type: string, id: string}) => (
+export const EditButton = ({type, id}: { type: string, id: string }) => (
     <Link className="btn btn-secondary btn-icon btn-sm" to={`/${type}/${id}`} css={{
-        textDecoration: 'none',
-        whiteSpace: 'nowrap',
+        textDecoration: "none",
+        whiteSpace: "nowrap",
     }}>
-        <ZeroIcon iconType={'solid'} iconName={'pencil'} className='fs-4 me-1 d-inline-block'/>
+        <i className="pi pi-eye"></i>
     </Link>
 )
