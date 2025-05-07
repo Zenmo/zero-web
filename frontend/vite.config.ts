50d4e15593c4e8c12793e8f6e3147c3f6d56a5e8
Erik van Velzen <erik@evanv.nl>
2025-05-07 15:35:13 +0200

    Avoid stale JavaScript caching

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,9 @@
 import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
+import {execSync} from "child_process"
+
+// Put hash in file name so that the users don't get stale JS from their browser cache.
+const treeHash = execSync("git rev-parse HEAD:frontend").toString().trim().slice(0, 4)
 
 export default defineConfig({
     plugins: [react({ jsxImportSource: '@emotion/react' })],
@@ -8,12 +12,12 @@ export default defineConfig({
         port: 3000,
     },
     build: {
-        minify: false,
+        // minify: false,
         rollupOptions: {
-            treeshake: false,
+            // treeshake: false,
             output: {
-                entryFileNames: "[name].js",
-                assetFileNames: '[name].css',
+                entryFileNames: `[name].${treeHash}.js`,
+                assetFileNames: `[name].${treeHash}.js`,
             },
         },
     },
